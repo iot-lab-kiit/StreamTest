62d7185bfbd3df6677168dacbee4721044f61590
Sahil Kumar <io.sahilkr@gmail.com>
2020-10-04 16:44:40 +0530

    replaced addition by multiplication

--- test.c
+++ test.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 int main() {    
 
-    int number1, number2, sum;
+    int number1, number2, mul;
     
     printf("Enter two integers: ");
     scanf("%d %d", &number1, &number2);
 
-    // calculating sum
-    sum = number1 + number2;      
+    // calculating product
+    mul = number1 * number2;      
     
-    printf("%d + %d = %d", number1, number2, sum);
+    printf("%d x %d = %d", number1, number2, mul);
     return 0;
 }
