a1c98079db91a33db8cd9d8316dede947a24965b
Sejal16 <sejal.august16@gmail.com>
2020-10-06 00:28:50 +0530

    Added few more operations to it

--- test.c
+++ test.c
@@ -8,7 +8,19 @@ int main() {
 
     // calculating product
     mul = number1 * number2;      
-    
     printf("%d x %d = %d", number1, number2, mul);
+
+    // Adding  two numbers
+    int add = number1 + number2;      
+    printf("%d + %d = %d", number1, number2, add);
+
+    //Subtracting two numbers
+    int sub = number1 - number2;      
+    printf("%d - %d = %d", number1, number2, sub);
+
+    //Dividing two numbers
+    double div = number1 % number2;      
+    printf("%d / %d = %f", number1, number2, div);
+
     return 0;
 }
